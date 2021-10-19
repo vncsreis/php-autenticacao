900074e334f9d2d5fc09d3815b51998615e19703
Vinícius Reis <88447626+vncsreis@users.noreply.github.com>
2021-10-19 00:33:58 -0300

    Correções em head.php

--- components/head.php
+++ components/head.php
@@ -1,9 +1,9 @@
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700&display=swap" rel="stylesheet">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700&display=swap" rel="stylesheet" />
   <title>Login</title>
-  <link rel="stylesheet" href="./css/auth.css">
+  <link rel="stylesheet" href="./css/auth.css" />
 </head>
